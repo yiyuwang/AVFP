a5b4530f36f2c4b49add8182ddd7c7ed8f712255
yiyuwang <yiyuwang6@gmail.com>
2020-10-15 17:16:01 -0400

    Update job scripts for discovery cluster

--- Preproc/run_fmriprep_batch_wrapper.sh
+++ Preproc/run_fmriprep_batch_wrapper.sh
@@ -6,7 +6,7 @@
 #SBATCH --tasks-per-node=1                        # sets 1 tasks for each machine
 #SBATCH --cpus-per-task=8                         # sets 8 core for each task
 #SBATCH --mem=100Gb                               # reserves 50 GB memory
-#SBATCH --partition=general                 	  # requests that the job is executed in partition general
+#SBATCH --partition=short                 	  	  # requests that the job is executed in partition short
 #SBATCH --time=23:59:00                           # reserves machines/cores for 20 hours.
 #SBATCH --output=fmriprep_AVFP.%j.out               # sets the standard output to be stored in file my_nice_job.%j.out, where %j is the job id)
 #SBATCH --error=fmriprep_AVFP.%j.err                # sets the standard error to be stored in file my_nice_job.%j.err, where %j is the job id)
